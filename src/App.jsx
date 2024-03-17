90f3c5df6a332ea5db2707da2d204101fac6734d
mendesdiego <diego.mendes7@outlook.com>
2024-03-17 09:44:05 -0300

    Adicionando os arquivos de estudos ao repositorio

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import './App.css'
+
+// 1 - Config react router
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+
+// Components
+import Navbar from './components/Navbar';
+
+// Pages
+import Home from './pages/Home';
+import About from './pages/About';
+import Product from './pages/Product';
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1>React Router</h1>
+        <BrowserRouter>
+          {/*2 - Links com react router */}
+          <Navbar />
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/about' element={<About />} />
+            {/* 4 - Rota dinamica */}
+            <Route path='/products/:id' element={Product} />
+          </Routes>
+        </BrowserRouter>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+// 2 - Links com react router
+import "./Navbar.css";
+
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <nav>
+      <Link to="/">Home</Link>
+      <Link to="/about">Sobre</Link>
+    </nav>
+  )
+}
+
+export default Navbar
