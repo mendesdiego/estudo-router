2241d15f3da51584d824c352f864847d4d612160
mendesdiego <diego.mendes7@outlook.com>
2024-03-18 20:22:08 -0300

    Atualizando arquivos carregando produto individualmente

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,12 +1,27 @@
 import { useParams } from "react-router-dom"
+import { useFetch } from "../hooks/useFetch";
 
 const Product = () => {
+  // 4 - Rota dinamica
   const { id } = useParams();
 
+  // 5 - Carregamento de dado individual
+  const url = 'http://localhost:3000/products/' + id;
+
+  const { data: product, loading, error } = useFetch(url);
+
   return (
-    <>
+    <div>
       <p>ID do produto: {id}</p>
-    </>
+      {error && <p>Ocorreu um erro...</p>}
+      {loading && <p>Carregnado...</p>}
+      {product && (
+        <div>
+          <h1>{product.name}</h1>
+          <p>R$ {product.price}</p>
+        </div>
+      )}
+    </div>
   )
 }
 
